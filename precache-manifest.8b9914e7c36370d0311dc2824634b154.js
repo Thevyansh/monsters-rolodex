5e2681a491f6deb3971d51632b4451aa90d2b22e
Thevyansh <thevyansh@gmail.com>
2019-11-22 10:56:30 +0530

    Updates

---
+++ precache-manifest.8b9914e7c36370d0311dc2824634b154.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29300cb2fe1a2b58580d964883497168",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d1b83f50675c294028b9",
+    "url": "/monsters-rolodex/static/css/main.9dee399a.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "d1b83f50675c294028b9",
+    "url": "/monsters-rolodex/static/js/main.9fb25594.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
